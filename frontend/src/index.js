11fc4bf4a95abfa40360fe33c5904865f9797573
WorkzbyRohan <149997054+WorkzbyRohan@users.noreply.github.com>
2025-04-18 00:01:19 -0700

    index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* Wrap the App component in BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 reportWebVitals();
