55ae7be40a9bf02bca341a892176c20bf87672be
WorkzbyRohan <149997054+WorkzbyRohan@users.noreply.github.com>
2025-04-18 00:00:41 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./pages/about";
-import Login from "./pages/Login";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./signup";
+import MainPage from "./MainPage";
+import TeamPage from "./TeamPage";
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        </Routes>
-  </BrowserRouter>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/main" component={MainPage} />
+        <Route path="/teams/:id" component={TeamPage} />
+      </Switch>
+    </Router>
   );
 }
 
