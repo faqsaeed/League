2d8166a9e2ce0a5cab2311f5a627953dbb8589c7
faqsaeed <faiqsaeed4@gmail.com>
2025-04-20 19:03:48 +0500

    Minor changes in authenttication

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const userRouter = express.Router();
 const { getUsers, createUser, updateUser, deleteUser } = require("../controllers/UserController");
-const {verifyToken, verifyAdmin, verifyAdminOrOwner, verifyOwnerForTeam} = require("../middleware/AuthMiddleware");
+const {verifyToken, verifyAdmin, verifyOwnerForTeam} = require("../middleware/AuthMiddleware");
 
-userRouter.get("/",verifyToken, verifyAdminOrOwner, getUsers);
+userRouter.get("/",verifyToken, verifyAdmin, getUsers);
 userRouter.post("/",verifyToken, verifyAdmin, createUser);
 userRouter.put("/:id",verifyToken, verifyOwnerForTeam, updateUser);
 userRouter.delete("/:id",verifyToken, verifyAdmin, deleteUser);
