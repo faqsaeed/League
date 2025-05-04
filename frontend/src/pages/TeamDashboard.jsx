d3c130be28600ef4e40404800e02e1219808c43e
faqsaeed <faiqsaeed4@gmail.com>
2025-05-04 16:37:24 +0500

    added function to let only admin to acces the page and corrced the redirecting routes

--- frontend/src/pages/TeamDashboard.jsx
+++ frontend/src/pages/TeamDashboard.jsx
@@ -1,19 +1,35 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link , useNavigate } from "react-router-dom";
+import adminCheck from '../services/adminCheck';
 import "../styles/TeamDashboard.css";
 
 function TeamDashboard() {
   const [teams, setTeams] = useState([]);
+  const navigate = useNavigate();
+  const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
+
+        const checkAdminStatus = () => {
+          const authStatus = adminCheck();
+          
+          if (!authStatus.isAuthenticated || !authStatus.isAdmin) 
+          {
+            navigate('/');
+          }
+          setIsAdmin(true);      
+        };
+        
+        checkAdminStatus();
+
     axios.get("http://localhost:5000/api/teams", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`
       }
     }).then(res => setTeams(res.data))
       .catch(err => console.error(err));
-  }, []);
+  }, [navigate]);
 
   const handleDelete = async (teamId) => {
     if (!window.confirm("Are you sure you want to delete this team?")) return;
@@ -33,13 +49,13 @@ function TeamDashboard() {
   return (
     <div className="admin-dashboard">
       <h2>Admin Dashboard</h2>
-      <Link to="/create-team" className="create-button">+ Create New Team</Link>
+      <Link to="/admin/createteam" className="create-button">+ Create New Team</Link>
       <ul className="team-list">
         {teams.map(team => (
           <li key={team.TeamID} className="team-card">
             <strong>{team.Name}</strong> - Coach: {team.Coach}
             <div className="team-actions">
-              <Link to={`/edit-team/${team.TeamID}`} className="edit-link">Edit</Link>
+              <Link to={`admmin/editteam/${team.TeamID}`} className="edit-link">Edit</Link>
               <button onClick={() => handleDelete(team.TeamID)} className="delete-button">Delete</button>
             </div>
           </li>
